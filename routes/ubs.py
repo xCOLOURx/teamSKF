ab5f6571c23c96d63a0e235449deecb05b1b96e1
EternalTempest <shane.ong.kheng.te@gmail.com>
2025-09-06 14:29:57 +0800

    try ubs again

--- routes/ubs.py
+++ routes/ubs.py
@@ -39,7 +39,7 @@ def universal_bureau_surv():
         res.append(ans)
         
         
-    return json.dumps({"networks": res})
+    return {"networks": res}
     
     
 # [{'networkId': '68456992-3fd6-4983-a35e-1f0a90f15c25', 'network': [{'spy1': 'Tony Stark', 'spy2': 'Natasha Romanoff'}, {'spy1': 'Tony Stark', 'spy2': 'Steve Rogers'}, {'spy1': 'Natasha Romanoff', 'spy2': 'Steve Rogers'}]}, {'networkId': 'fee2ea9d-7af5-4133-b1f4-7222373026ef', 'network': [{'spy1': 'Reed Richards', 'spy2': 'Ben Grimm'}, {'spy1': 'Sue Storm', 'spy2': 'Ben Grimm'}, {'spy1': 'Johnny Storm', 'spy2': 'Ben Grimm'}, {'spy1': 'Johnny Storm', 'spy2': 'Sue Storm'}]}, {'networkId': '3fd489bc-3710-491e-82ab-e78b67da9543', 'network': [{'spy1': 'Erik Lensherr', 'spy2': 'Scott Summers'}, {'spy1': 'Scott Summers', 'spy2': 'Ororo Munroe'}, {'spy1': 'Charles Xavier', 'spy2': 'Ororo Munroe'}, {'spy1': 'Scott Summers', 'spy2': 'Jean Grey'}, {'spy1': 'Ororo Munroe', 'spy2': 'Jean Grey'}]}]
