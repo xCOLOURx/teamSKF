fe0b2d8cd35aa87821e06b9f8ff46aa48d44473f
xCOLOURx <kylezcc@gmail.com>
2025-09-07 00:26:57 +0800

    trivia

--- routes/trivia.py
+++ routes/trivia.py
@@ -10,6 +10,6 @@ logger = logging.getLogger(__name__)
 
 @app.route('/trivia', methods=['GET'])
 def evaluate():
-    data = {  "answers": [4, ]};
+    data = {  "answers": [4, 1, 2, 2, 3, 4, 1, 5, 4, 3, 3, 3, 4, 1, 2, 1, 1, 2, 2, 1, 2, 2, 3, 4, 2] }
     logging.info("My result :{}".format(data))
     return json.dumps(data)
