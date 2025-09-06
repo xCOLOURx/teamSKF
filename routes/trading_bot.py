388a2c62e0612b2b4f680a5784e5fd85423461a9
fuwentay <fuwentay@gmail.com>
2025-09-06 22:19:53 +0800

    debugging

--- routes/trading_bot.py
+++ routes/trading_bot.py
@@ -33,36 +33,36 @@ def trading_bot():
 
     lst_strict = [d for d in lst_strict if d] # Remove empty dicts
 
-    # TODO: For the remaining balance, take a more lax condition
-    lst_lax = []
-    for index in data:
-        dict = {}
-        title = index["title"]
-        previous_candles = index["previous_candles"]
-        observation_candles = index["observation_candles"]
+    # # TODO: For the remaining balance, take a more lax condition
+    # lst_lax = []
+    # for index in data:
+    #     dict = {}
+    #     title = index["title"]
+    #     previous_candles = index["previous_candles"]
+    #     observation_candles = index["observation_candles"]
 
-        # Taken a stricter condition
-        if (observation_candles[0]["volume"] < observation_candles[1]["volume"] < observation_candles[2]["volume"]):
-            if (observation_candles[0]["close"] < observation_candles[1]["close"] < observation_candles[2]["close"]):
-                dict["decision"] = "LONG"
-                dict["diff"] = observation_candles[2]["close"] - observation_candles[0]["close"]
-                dict["id"] = index["id"]
-            elif (observation_candles[0]["close"] > observation_candles[1]["close"] > observation_candles[2]["close"]):
-                dict["decision"] = "SHORT"
-                dict["diff"] = observation_candles[0]["close"] - observation_candles[2]["close"]
-                dict["id"] = index["id"]
+    #     # Taken a stricter condition
+    #     if (observation_candles[0]["volume"] < observation_candles[1]["volume"] < observation_candles[2]["volume"]):
+    #         if (observation_candles[0]["close"] < observation_candles[1]["close"] < observation_candles[2]["close"]):
+    #             dict["decision"] = "LONG"
+    #             dict["diff"] = observation_candles[2]["close"] - observation_candles[0]["close"]
+    #             dict["id"] = index["id"]
+    #         elif (observation_candles[0]["close"] > observation_candles[1]["close"] > observation_candles[2]["close"]):
+    #             dict["decision"] = "SHORT"
+    #             dict["diff"] = observation_candles[0]["close"] - observation_candles[2]["close"]
+    #             dict["id"] = index["id"]
 
-        lst_lax.append(dict)
+    #     lst_lax.append(dict)
     
-    # Remove indexes in lst_lax that are already in lst_strict by matching 'id'
-    strict_ids = {d['id'] for d in lst_strict if 'id' in d}
-    lst_lax = [d for d in lst_lax if d.get('id') not in strict_ids]
-    # Now filter lst_lax to top 50 by 'diff' value
-    lst_lax = sorted(lst_lax, key=lambda d: d.get('diff', float('-inf')), reverse=True)[:(50-len(lst_strict))]
+    # # Remove indexes in lst_lax that are already in lst_strict by matching 'id'
+    # strict_ids = {d['id'] for d in lst_strict if 'id' in d}
+    # lst_lax = [d for d in lst_lax if d.get('id') not in strict_ids]
+    # # Now filter lst_lax to top 50 by 'diff' value
+    # lst_lax = sorted(lst_lax, key=lambda d: d.get('diff', float('-inf')), reverse=True)[:(50-len(lst_strict))]
 
-    lst = lst_strict + lst_lax
+    # lst = lst_strict + lst_lax
     
-    # lst = lst_strict
+    lst = lst_strict
 
     for d in lst:
         d.pop('diff', None)
