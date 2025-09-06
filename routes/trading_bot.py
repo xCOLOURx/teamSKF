b091c4237162df31d400ef516ecefec2fbfbce07
fuwentay <fuwentay@gmail.com>
2025-09-06 18:24:04 +0800

    stricter strategy

--- routes/trading_bot.py
+++ routes/trading_bot.py
@@ -18,22 +18,26 @@ def trading_bot():
         previous_candles = index["previous_candles"]
         observation_candles = index["observation_candles"]
 
-        if (observation_candles[0]["volume"] < observation_candles[1]["volume"] < observation_candles[2]["volume"]):
-            if (observation_candles[0]["close"] < observation_candles[1]["close"] < observation_candles[2]["close"]):
+        # TODO: improve strategy by being stricter -- taking in previous candles
+        if (previous_candles[2]["volume"] < observation_candles[0]["volume"] < observation_candles[1]["volume"] < observation_candles[2]["volume"]):
+            if (previous_candles[2]["close"] < observation_candles[0]["close"] < observation_candles[1]["close"] < observation_candles[2]["close"]):
                 dict["decision"] = "LONG"
                 dict["diff"] = observation_candles[2]["close"] - observation_candles[0]["close"]
-            elif (observation_candles[0]["close"] > observation_candles[1]["close"] > observation_candles[2]["close"]):
+            elif (previous_candles[2]["close"] > observation_candles[0]["close"] > observation_candles[1]["close"] > observation_candles[2]["close"]):
                 dict["decision"] = "SHORT"
                 dict["diff"] = observation_candles[0]["close"] - observation_candles[2]["close"]
-                # TODO: sort by price diff & select 50 best. can add other indicators
 
         dict["id"] = index["id"]
         lst.append(dict)
+        print(f"Number of potential trades: {len(lst)}")
     
     # Filter lst to top 50 by 'diff' value
     lst = sorted(lst, key=lambda d: d.get('diff', float('-inf')), reverse=True)[:50]
     for d in lst:
         d.pop('diff', None)
+    
+    print(f"Number of potential trades after filtering top 50: {len(lst)}")
+
 
     print(lst)
     return json.dumps(lst)
