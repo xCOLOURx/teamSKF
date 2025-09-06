3b6cc9058cb8882bc41b09225f534129b6b4e33b
EternalTempest <shane.ong.kheng.te@gmail.com>
2025-09-06 10:12:44 +0800

    princess diaries input

---
+++ routes/princess_diaries.py
@@ -0,0 +1,20 @@
+import json
+import logging
+
+from flask import request
+
+from routes import app
+
+logger = logging.getLogger(__name__)
+
+
+@app.route('/princess-diaries', methods=['POST'])
+def evaluate():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    tasks = data.get("tasks")
+    subway = data.get("subway")
+    starting_station = int(data.get("starting_station"))
+    print(tasks)
+    print(subway)
+    print(starting_station)
