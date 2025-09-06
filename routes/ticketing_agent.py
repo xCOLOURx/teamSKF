4d6b68d91f81c6dfe3af36ba3ecab63f791aa99e
EternalTempest <shane.ong.kheng.te@gmail.com>
2025-09-07 06:01:44 +0800

    ticketing agent

--- routes/ticketing_agent.py
+++ routes/ticketing_agent.py
@@ -17,7 +17,25 @@ def ticketing_agent():
     customers = data["customers"]
     concerts = data["concerts"]
     priority = data["priority"]
-    
-    
-                
+    ans = {}
+    for customer in customers:
+        vip_bonus = 100 if customer["vip_status"] else 0
+        points = {concert["name"]:vip_bonus for concert in concerts}
+        points[priority[customer["credit_card"]]] += 50
+        ax, ay = customer["location"][0], customer["location"][1]
+        for concert in concerts:
+            bx,by = concert["booking_center_location"][0], concert["booking_center_location"][1]
+            dist = abs(ax-bx) + abs(ay-by)
+            points[concert["name"]] += max(0, 30 - 5*(dist-2))
+        ans[customer["name"]] = max(points.items(), key=lambda x: x[1])[0]
+    return ans
+        
+    # dist 2 = 30 points
+    # dist 3 = 25 points
+    # dist 4 = 20 points
+    # dist 5 = 15 points
+    # dist 6 = 10 points
+    # dist 7 = 5 points
+    # dist 8 = 0 points
+                 
 
